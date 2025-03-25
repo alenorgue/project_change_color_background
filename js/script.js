74d5d3e12e9c0123326afc68546c01cbb21785fa
alenorgue <alenoriegag@gmail.com>
2025-03-25 16:55:53 +0100

    finished both iterations

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 //En este array de strings, tenemos todos los colores
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple'];
+let i = 0;
+
+const myButton = document.querySelector('.btn.btn-outline-secondary');
+const myBody = document.querySelector('body');
+
+   //myButton.addEventListener('click', () => {
+ //   myBody.style.backgroundColor = 'red';});
 
 
+myButton.addEventListener('click', () => {
+    myBody.style.backgroundColor = colors[i];
+    i = (i + 1) % colors.length;});
+    
+    
+ 
